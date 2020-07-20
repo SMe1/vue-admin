3a6bae262725101bd8d41426f363029d9dff6ecb
SMe1 <1850748095@qq.com>
2020-07-20 15:36:21 +0800

    项目初始化

---
+++ src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="login">
+        <el-button></el-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style lang="scss" scoped>
+    #login{
+        height: 100vh;
+        background: #344a5f;
+    }
+</style>
